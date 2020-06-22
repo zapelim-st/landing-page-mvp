96ba2dc81939ebb2a27cc668f690e3910d286514
zapelim <zapelim-dev@outlook.com>
2020-06-22 14:14:45 -0300

    landing page v1

---
+++ run.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return render_template('zapelim.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
